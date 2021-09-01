725b2b95e4637ce0e68ab4475aa3939f985d5421
NikMerkulov <RikoStation@gmail.com>
2021-09-02 01:06:52 +0700

    week-5-day-3-pt2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import './App.css'
+import styles from './App.module.css'
 import TextList from './components/week-5-day-3/TextList'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom'
+import PartTwo from './components/week-5-day-3-pt2/PartTwo'
 
 const App = () => {
   return (
-    <div className="App">
-      <TextList />
+    <div className={styles.App}>
+      <Router>
+        <div className={styles.header}>
+          <Link to='/TextList'>Text List</Link>
+          <Link to='/PartTwo'>Part Two</Link>
+        </div>
+        <Switch>
+          <Route path="/TextList">
+            <TextList />
+          </Route>
+          <Route path="/PartTwo">
+            <PartTwo />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   )
 }
